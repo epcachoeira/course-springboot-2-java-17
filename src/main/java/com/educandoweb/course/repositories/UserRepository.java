ead706eb1a1bdc7d26fab24dc5c64466ece67532
epcachoeira <epcachoeira@hotmail.com>
2021-11-25 16:26:46 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+// @Repository  // Notação não necessária por conta do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
